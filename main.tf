51c3d188f5942f99e0db2e31d46a3e81df0d7953
Jayesh Dhopte <jayeshdhopte66@gmail.com>
2026-03-09 19:32:00 +0530

    changed the main.tf file

--- main.tf
+++ main.tf
@@ -1,40 +1,41 @@
+# 1. Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = var.resource_group
+  location = var.resource_group_region
   tags = {
     Environment = var.environment
   }
 }
- 
-# Virtual Network and Subnet
+
+# 2. Virtual Network and Subnet
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  address_space       = var.address_space
+  name                = var.virtual_network_name
+  address_space       = var.vnet_address_prefix
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
- 
+
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  address_prefixes     = var.subnet_prefix
+  address_prefixes     = var.subnet_address_prefix
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
 }
- 
-# Public IP
+
+# 3. Public IP
 resource "azurerm_public_ip" "pip" {
-  name                = "${var.vm_name}-pip"
+  name                = "${var.virtual_machine_name}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
- 
-# Network Security Group
+
+# 4. Network Security Group
 resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.vm_name}-nsg"
+  name                = "${var.virtual_machine_name}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
- 
+
   security_rule {
     name                       = "Allow-SSH"
     priority                   = 100
@@ -47,13 +48,13 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
- 
-# Network Interface
+
+# 5. Network Interface
 resource "azurerm_network_interface" "nic" {
   name                = var.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
- 
+
   ip_configuration {
     name                          = var.ip_config_name
     subnet_id                     = azurerm_subnet.subnet.id
@@ -62,30 +63,32 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
- 
+
+# 6. Associate NSG to NIC
 resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
- 
+
+# 7. Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = var.vm_name
+  name                  = var.virtual_machine_name
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   size                  = var.vm_size
   admin_username        = var.admin_username
   network_interface_ids = [azurerm_network_interface.nic.id]
- 
+
   admin_ssh_key {
     username   = var.admin_username
     public_key = file(var.ssh_public_key_path)
   }
- 
+
   os_disk {
     caching              = var.os_disk_caching
     storage_account_type = var.os_disk_storage_type
   }
- 
-  # Dynamically points to your Azure Compute Gallery Image
-  source_image_id = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Compute/galleries/${var.gallery_name}/images/${var.image_name}/versions/${var.image_version}"
+
+  # Note: Ensure gallery_name, image_name, and image_version are defined in variables.tf
+  source_image_id = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}/providers/Microsoft.Compute/galleries/${var.gallery_name}/images/${var.image_name}/versions/${var.image_version}"
 }
