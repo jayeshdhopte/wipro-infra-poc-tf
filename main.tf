c404a75ef46b0533015f0798f323fe87da36824b
madhutranslab <150128572+madhutranslab@users.noreply.github.com>
2026-03-25 16:43:31 +0530

    updated code

---
+++ main.tf
@@ -0,0 +1,137 @@
+data "azurerm_resource_group" "rg" {
+  count = var.rg_choice == "Exists" ? 1 : 0
+  name  = var.resource_group
+}
+
+resource "azurerm_resource_group" "rg" {
+  count    = var.rg_choice == "Do Not Exist" ? 1 : 0
+  name     = var.resource_group
+  location = var.resource_group_region
+}
+
+data "azurerm_virtual_network" "vnet" {
+  count               = var.vnet_choice == "Exists" ? 1 : 0
+  name                = var.virtual_network_name
+  resource_group_name = local.rg_name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  count               = var.vnet_choice == "Do Not Exist" ? 1 : 0
+  name                = var.virtual_network_name
+  location            = local.rg_location
+  resource_group_name = local.rg_name
+  address_space       = var.vnet_address_prefix
+}
+
+
+
+data "azurerm_subnet" "subnet" {
+  count                = var.subnet_choice == "Exists" ? 1 : 0
+  name                 = var.subnet_name
+  virtual_network_name = local.vnet_name
+  resource_group_name  = local.rg_name
+}
+
+resource "azurerm_subnet" "subnet" {
+  count                = var.subnet_choice == "Do Not Exist" ? 1 : 0
+  name                 = var.subnet_name
+  resource_group_name  = local.rg_name
+  virtual_network_name = local.vnet_name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+
+data "azurerm_network_security_group" "nsg" {
+  count               = var.nsg_choice == "Exists" ? 1 : 0
+  name                = var.nsg_name
+  resource_group_name = local.rg_name
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  count               = var.nsg_choice == "Do Not Exist" ? 1 : 0
+  name                = var.nsg_name
+  location            = local.rg_location
+  resource_group_name = local.rg_name
+}
+
+resource "azurerm_network_security_rule" "rule" {
+  count                       = var.nsg_choice == "Do Not Exist" ? 1 : 0
+  name                        = var.rule_name
+  priority                    = var.rule_priority
+  direction                   = var.rule_direction
+  access                      = var.rule_access
+  protocol                    = var.rule_protocol
+  source_port_range           = var.source_port_range
+  destination_port_range      = var.destination_port_range
+  source_address_prefix       = var.nsg_source_address_prefix
+  destination_address_prefix  = var.nsg_destination_address_prefix
+  resource_group_name         = local.rg_name
+  network_security_group_name = azurerm_network_security_group.nsg[0].name
+}
+
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = var.public_ip_name
+  location            = local.rg_location
+  resource_group_name = local.rg_name
+  allocation_method   = var.pip_allocation_method
+}
+
+data "azurerm_network_interface" "nic" {
+  count               = var.nic_choice == "Exists" ? 1 : 0
+  name                = var.nic_name
+  resource_group_name = local.rg_name
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = var.nic_choice == "Do Not Exist" ? 1 : 0
+  name                = var.nic_name
+  location            = local.rg_location
+  resource_group_name = local.rg_name
+
+  ip_configuration {
+    name                          = var.ip_config_name
+    subnet_id                     = local.subnet_id
+    private_ip_address_allocation = var.private_ip_allocation
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+data "azurerm_shared_image_version" "image" {
+  count               = var.use_shared_image ? 1 : 0
+  name                = var.shared_image_version
+  image_name          = var.image_definition_name
+  gallery_name        = var.shared_gallery
+  resource_group_name = local.rg_name
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = var.os_disk_choice == "Do Not Exist" ? 1 : 0
+  name                = var.virtual_machine_name
+  location            = local.rg_location
+  resource_group_name = local.rg_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [local.nic_id]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  source_image_reference {
+    publisher = var.operating_system_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_storage_account_type
+  }
+}
+
---
+++ varibles.tf
@@ -0,0 +1,98 @@
+variable "rg_choice" { type = string }
+variable "vnet_choice" { type = string }
+variable "subnet_choice" { type = string }
+variable "nsg_choice" { type = string }
+variable "nic_choice" { type = string }
+variable "os_disk_choice" { type = string }
+
+variable "resource_group" { type = string }
+variable "resource_group_region" { type = string }
+
+variable "virtual_network_name" { type = string }
+variable "vnet_address_prefix" { type = list(string) }
+
+variable "subnet_name" { type = string }
+variable "subnet_address_prefix" { type = list(string) }
+
+variable "nsg_name" { type = string }
+
+variable "rule_name" { type = string }
+variable "rule_priority" { type = number }
+variable "rule_direction" { type = string }
+variable "rule_access" { type = string }
+variable "rule_protocol" { type = string }
+variable "source_port_range" { type = string }
+variable "destination_port_range" { type = string }
+variable "nsg_source_address_prefix" { type = string }
+variable "nsg_destination_address_prefix" { type = string }
+
+variable "public_ip_name" { type = string }
+variable "pip_allocation_method" { type = string }
+
+variable "nic_name" { type = string }
+variable "ip_config_name" { type = string }
+variable "private_ip_allocation" { type = string }
+
+variable "os_managed_disk_id" { type = string }
+variable "os_disk_caching" { type = string }
+variable "os_storage_account_type" { type = string }
+
+# ---------- Shared Image ----------
+variable "shared_image_version" {
+  description = "Version of the shared image"
+  type        = string
+}
+
+variable "image_definition_name" {
+  description = "Name of the image definition"
+  type        = string
+}
+
+variable "shared_gallery" {
+  description = "Name of the shared image gallery"
+  type        = string
+}
+
+variable "use_shared_image" {
+  description = "Flag to use shared image"
+  type        = bool
+  default     = false
+}
+
+# ---------- VM ----------
+variable "virtual_machine_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the VM"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+}
+
+# ---------- Image Reference ----------
+variable "operating_system_publisher" {
+  description = "Image publisher (e.g., RedHat)"
+  type        = string
+}
+
+variable "image_offer" {
+  description = "Image offer (e.g., RHEL)"
+  type        = string
+}
+
+variable "image_sku" {
+  description = "Image SKU"
+  type        = string
+}
+
