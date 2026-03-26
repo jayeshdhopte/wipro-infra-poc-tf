b30e47c6cbda530ad0c24b0a2b9cad2c0f971eca
madhutranslab <150128572+madhutranslab@users.noreply.github.com>
2026-03-26 11:47:52 +0530

    code change

--- Azure testing/locals.tf
+++ Azure testing/locals.tf
@@ -1,12 +1,10 @@
 locals {
-  rg_name     = var.rg_choice == "Exists" ? data.azurerm_resource_group.rg[0].name : azurerm_resource_group.rg[0].name
-  rg_location = var.rg_choice == "Exists" ? data.azurerm_resource_group.rg[0].location : azurerm_resource_group.rg[0].location
+  rg_name     = try(data.azurerm_resource_group.rg[0].name, azurerm_resource_group.rg[0].name)
+  rg_location = try(data.azurerm_resource_group.rg[0].location, azurerm_resource_group.rg[0].location)
 
-  vnet_name = var.vnet_choice == "Exists" ? data.azurerm_virtual_network.vnet[0].name : azurerm_virtual_network.vnet[0].name
+  vnet_name = try(data.azurerm_virtual_network.vnet[0].name, azurerm_virtual_network.vnet[0].name)
 
-  subnet_id = var.subnet_choice == "Exists" ? data.azurerm_subnet.subnet[0].id : azurerm_subnet.subnet[0].id
+  subnet_id = try(data.azurerm_subnet.subnet[0].id, azurerm_subnet.subnet[0].id)
 
-  nsg_id = var.nsg_choice == "Exists" ? data.azurerm_network_security_group.nsg[0].id : azurerm_network_security_group.nsg[0].id
-
-  nic_id = var.nic_choice == "Exists" ? data.azurerm_network_interface.nic[0].id : azurerm_network_interface.nic[0].id
+  nic_id = try(data.azurerm_network_interface.nic[0].id, azurerm_network_interface.nic[0].id)
 }
